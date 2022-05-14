ffe8d5d735036b7b947c0d8f6418b584a98aabef
kingstan070 <allwinkingstan2002@gmail.com>
2022-05-14 17:58:40 +0000

    Started to write the main program

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-
+import os
 import sys
 
 import Checker.standard_checker as a
 import scrapper.find_link as fl
 
-print(a.check4plagirism("src/text1.txt","src/text2.txt"))
+from rich import print as rprint
+
+banner = '''
+ _____                  ______ _           _ _____       _   
+/  __ \                 |  ___(_)         | /  __ \     | |  
+| /  \/ ___  _ __  _   _| |_   _ _ __   __| | /  \/ __ _| |_ 
+| |    / _ \| '_ \| | | |  _| | | '_ \ / _` | |    / _` | __|
+| \__/| (_) | |_) | |_| | |   | | | | | (_| | \__/| (_| | |_ 
+ \____/\___/| .__/ \__, \_|   |_|_| |_|\__,_|\____/\__,_|\__|
+            | |     __/ |                                    
+            |_|    |___/                                     
+
+
+'''
+print(sys.argv[1])
+
+f_locationTEST = "src/text1.txt"
+f_locationDUPL = "src/text2.txt"
+
+os.system('cls||clear')
+print(banner)
+
+def write_file(data, f_location):
+    pass
+#print(a.check4plagirism("src/text1.txt","src/text2.txt"))
 
 #links = fl.get_url("Plagirism detector explained")
 #for i in range(int(sys.argv[1])):
